4a0606957b727882fb10fe796e1fb20476bbc538
Daniel Lehmann <139554573+lehmand@users.noreply.github.com>
2025-10-28 13:08:10 +0000

    First detailed commit
    This is a test commit to test how it looks when we write detailed commit messages

--- chat_api/serializers.py
+++ chat_api/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Channel
 
 class BasicChannelListSerializer(serializers.ModelSerializer):
+    """Serializer for basic channel list"""
 
     class Meta:
         model = Channel
